80ec6d8b506a807ed5c35d9a961f2ba71ebc7714
Akihiko WATANABE <gj.13a.w@gmail.com>
2016-08-18 00:15:44 +0900

    write train method

--- scripts/nmt_attention.py
+++ scripts/nmt_attention.py
@@ -2,14 +2,60 @@
 
 import argparse
 from lib.backup import Backup
+from lib.vocab import Vocab
+from lib.models import AttentionBasedEncoderDecoder as ABED
+from lib.generators import word_list, batch, sort
+from chainer import serializers
+
+HPARAM_NAME = "hyper_params"
+TRG_VOCAB_NAME = "trgvocab"
+SRC_VOCAB_NAME = "srcvocab"
+
+def forward():
+
+def train(args):
+    source_vocab = Vocab(args.source, args.vocab)
+    target_vocab = Vocab(args.target, args.vocab)
+    att_encdec = ABED(args.vocab, args.hidden_size, args.maxout_hidden_size, args.embed_size)
+    if args.use_gpu:
+        att_encdec.to_gpu()
+    for epoch in xrange(args.epochs):
+        print "--- epoch: %s/%s ---"%(epoch+1, args.epochs)
+        source_gen = word_list(args.source)
+        target_gen = word_list(args.target)
+        batch_gen = batch(sort(source_gen, target_gen, 100*args.minibatch), args.minibatch)
+        opt = optimizers.AdaDelta(args.rho, args.eps)
+        opt.setup(att_encdec)
+        n = 0
+        for source_batch, target_batch in batch_gen:
+            n += len(source_batch)
+            source_batch = fill_batch_end(source_batch)
+            target_batch = fill_batch_end(target_batch)
+            hyp_batch, loss = forward()
+            
+            loss.backward()
+            opt.weight_decay(0.001)
+            opt.update()
+        prefix = args.model_path + '%s'%(epoch+1)
+        serializers.save_hdf5(prefix+'.attencdec', att_encdec)
+    hyp_params = att_encdec.get_hyper_params()
+    Backup.dump(args.model_path+HPARAM_NAME)
+    source_vocab.save(args.model_path+SRC_VOCAB_NAME)
+    target_vocab.save(args.model_path+TRG_VOCAB_NAME)
+
+def test(args):
+    
 
 def parse_args():
     # each default parameter is according to the settings of original paper.
     DEF_EPOCHS = 10
-    DEF_EMBED = 600
+    DEF_EMBED = 620
     DEF_MINIBATCH = 80
     DEF_HIDDEN = 1000
+    DEF_MAXOUT_HIDDEN = 500
     DEF_VOCAB = 30000
+    DEF_EPS = 1e-06
+    DFE_RHO = 0.95
 
     p = argparse.ArgumentParser(
         description = "A Neural Attention Model for Machine Translation"
@@ -57,10 +103,16 @@ def parse_args():
             help="size of word embedding"
             )
     p.add_argument(
-            "hidden_size",
+            "-hidden_size",
             type=int,
             default=DEF_HIDDEN,
             help="# of hidden units"
+            )
+    p.add_argument(
+            "-maxout_hidden_size",
+            type=int,
+            default=DEF_MAXOUT_HIDDEN,
+            help="# of maxout hidden units"
             )
     p.add_argument(
             "-minibatch",
@@ -74,6 +126,26 @@ def parse_args():
             default=DEF_VOCAB,
             help="size of vocabulary"
             )
+    p.add_argument(
+            "-rho",
+            type=int,
+            default=DEF_RHO,
+            help="rho of AdaDelta"
+            )
+    p.add_argument(
+            "-eps",
+            type=int,
+            default=DEF_EPS,
+            help="epsilon of AdaDelta"
+            )
     args = p.parse_args()
 
     return args
+
+if __name__=='__main__':
+    args = parse_args()
+    XP.set_library(args.use_gpu)
+    if args.train:
+        train(args)
+    elif args.test:
+        test(args)
